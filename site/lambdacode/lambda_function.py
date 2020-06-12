05151531fd611d55ca9fb95082170c575a6dc83a
Parihar <mparih@amazon.com>
2020-06-12 17:38:23 +0530

    changed file to combine results according to commit_id

--- site/lambdacode/lambda_function.py
+++ site/lambdacode/lambda_function.py
@@ -3,13 +3,13 @@ import json
 import requests
 from requests_aws4auth import AWS4Auth
 
-region = 'us-east-1' # For example, us-west-1
+region = 'us-east-1'
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
 
-host = 'https://search-localization-of-bug-p6axgikx4z4jnqiywdpafqce6m.us-east-1.es.amazonaws.com' # For example, search-mydomain-id.us-west-1.es.amazonaws.com
-index = 'test'
+host = 'search-localization-of-bug-p6axgikx4z4jnqiywdpafqce6m.us-east-1.es.amazonaws.com' # For example, search-mydomain-id.us-west-1.es.amazonaws.com
+index = 'test1'
 url = 'https://' + host + '/' + index + '/_search'
 
 # Lambda execution starts here
@@ -18,30 +18,58 @@ def handler(event, context):
     # Put the user query into the query DSL for more accurate search results.
     # Note that certain fields are boosted (^).
     query = {
-        "size": 25,
-        "query": {
-            "function_score": {
-                "query": {
-                  "multi_match" : {
-                  "query": event['queryStringParameters']['q']
-                  }
-                },
-                "functions": [
-                  {
-                    "exp": {
-                    "file_date": {
-                          "scale": "10d",
-                          "offset": "2d", 
-                          "decay" : 0.6
-                        }
-                      },
-                      "weight": 1
-                    }
-                  ],
-                  "boost_mode": "multiply"
+        "size": 0, 
+  "query": {
+        "function_score": {
+            "query": {
+              "multi_match" : {
+              "query": event['queryStringParameters']['q'],
+              "fields": ["file_name^4", "entities^2", "commit_msg", "diff_file"],
               }
+            },
+            "functions": [
+              {
+                "exp": {
+                "file_date": {
+                      "scale": "10d",
+                      "offset": "2d", 
+                      "decay" : 0.6
+                    }
+                  },
+                  "weight": 1
+                }
+              ],
+              "boost_mode": "multiply"
+          }
+    },
+  "aggs": {
+    "combine_commit": {
+      "terms": {
+        "field": "commit_id",
+        "order": {
+          "top_hit": "desc"
         }
+      },
+      "aggs": {
+        "top_commits": {
+          "top_hits": {
+            "_source": {
+              "includes": ["package_link","commit_msg","file_date"]
+            },
+            "size": 1
+          }
+        },
+        "top_hit":{
+          "max": {
+            "script":{
+              "source": "_score"
+            }
+          }
+        }
+      }
     }
+  }
+}
 
     # ES 6.x requires an explicit Content-Type header
     headers = { "Content-Type": "application/json" }
