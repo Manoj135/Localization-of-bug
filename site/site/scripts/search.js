10d0b31ef107b2602fdea4f1f0316175b6f7e46a
Parihar <mparih@amazon.com>
2020-06-12 17:35:27 +0530

    changed file to combine results according to a commit_id

--- site/site/scripts/search.js
+++ site/site/scripts/search.js
@@ -23,9 +23,6 @@ button.click(function(){
 async function search() {
   // Clear results before searching
   noresults.hide();
-  // table.dataTable().Clear();
-  // table.dataTable().Draw();
-  // table.dataTable().Destroy();
   table.empty();
   resultdiv.empty();
   loadingdiv.show();
@@ -36,31 +33,31 @@ async function search() {
     // Make the HTTP request with the query as a parameter and wait for the JSON results
     let response = await $.get(apigatewayendpoint, { q: query, size: 25 }, 'json');
     // Get the part of the JSON response that we care about
-    let results = response['hits']['hits'];
+    let results = response['aggregations']['combine_commit']['buckets'];
+    // let results = response['hits']['hits'];
     if (results.length > 0) {
       loadingdiv.hide();
       // Iterate through the results and write them to HTML
-      resultdiv.append('<a href=\"https://www.google.com/\">Found ' + results.length + ' results.</a>');
-      table.append('<thead><tr><th>Rank</th><th>Score</th><th>CommitId</th><th>CommitMessage</th><th>CommitDate</th><th>FileName</th></tr></thead><tbody>');
+      resultdiv.append('<a href="https://www.google.com/">Found ' + results.length + ' results.</a>');
+      table.append('<thead><tr><th>Rank</th><th>Score</th><th>CommitId</th><th>CommitMessage</th><th>CommitDate</th></tr></thead><tbody>');
 
       for (var item in results) {
-        let url = results[item]._id;
-        let score = results[item]._score;
-        let commitid = results[item]._source.commit_id;
-        let fileName = results[item]._source.file_name;
-        let fileDate = results[item]._source.file_date;
-        let commitmsg = results[item]._source.commit_msg;
-        let packageLink = results[item]._source.package_link;
+        let Score = results[item].top_hit.value;
+        let commitid = results[item].key;
+        let count = results[item].doc_count;
+        // let fileName = results[item]._source.file_name;
+        let fileDate = results[item].top_commits.hits.hits[0]._source.file_date;
+        let commitmsg = results[item].top_commits.hits.hits[0]._source.commit_msg;
+        let packageLink = results[item].top_commits.hits.hits[0]._source.package_link;
+        var score = jQuery.trim(Score).substring(0, 8);
         var commitMessage;
         var commitId = jQuery.trim(commitid).substring(0, 7);
-        if(commitmsg.length>100){
-          commitMessage = jQuery.trim(commitmsg).substring(0, 100)
+        if(commitmsg.length>200){
+          commitMessage = jQuery.trim(commitmsg).substring(0, 200)
           .split(" ").slice(0, -1).join(" ") + "...";}
         else {commitMessage = commitmsg;}
         // Construct the full HTML string that we want to append to the div
-        table.append( '<tr><td>'+(++item)+'</td><td>'+score+'</td><td><a href='+packageLink+'/commits/'+commitid+' target="_blank" >' + commitId + '</a></td><td> '+commitMessage+'</td><td>' + fileDate + ' </td><td>' + fileName + '</td></tr>' );
-        // resultdiv.append('<div class="result">' +
-        // '<div><h1>'+(++item)+' score: '+score+'</h1><h2><p> commit_id: ' + commitId + '</p></h2><p> file_date: ' + fileDate + ' </p><p> file_name: ' + fileName + '</p></div></div>');
+        table.append( '<tr><td>'+(++item)+'</td><td>'+score+'</td><td><a href='+packageLink+'/commits/'+commitid+' target="_blank" >' + commitId+'('+count+')' + '</a></td><td> '+commitMessage+'</td><td>' + fileDate + ' </td></tr>' );
       }
       table.append('</tbody>');
       // table.hide();
@@ -75,8 +72,3 @@ async function search() {
   }
   loadingdiv.hide();
 }
-
-// Tiny function to catch images that fail to load and replace them
-// function imageError(image) {
-//   image.src = 'images/no-image.png';
-// }
